1028182a972a97d66d129aa3c3b9bd8e45b0e946
branila <stefan.branila55@gmail.com>
2025-02-26 20:03:17 +0100

    project cleanup

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,7 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core'
 
+// Via 'sta cosa!
 export const user = pgTable('user', {
 	id: serial('id').primaryKey(),
 	age: integer('age'),
-});
+})
